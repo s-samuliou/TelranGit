3c310bae83c59d9ec60f86eb8e050279455c1fac
stsiapan <shtefsv@gmail.com>
2023-11-22 22:24:17 +0300

    add new actions

--- main/org/lessonthree/Program.java
+++ main/org/lessonthree/Program.java
@@ -4,11 +4,13 @@ public class Program {
 
     public static void main(String[] args) {
         loop(100);
+        loop(200);
+        loop(300);
     }
 
     public static void loop(int count) {
         while (count > 0) {
-            System.out.println("Count: " + count);
+            System.out.println("Count is: " + count);
             count--;
         }
     }
