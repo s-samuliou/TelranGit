c09887425b25fcd5f23ae9cf6bdb2d45bd812d59
stsiapan <shtefsv@gmail.com>
2023-11-22 22:04:09 +0300

    something test commit

---
+++ main/org/lessonthree/Program.java
@@ -0,0 +1,14 @@
+package org.lessonthree;
+
+public class Program {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static void loop(int count) {
+        while (count > 0) {
+            System.out.println("Count: " + count);
+        }
+    }
+}
